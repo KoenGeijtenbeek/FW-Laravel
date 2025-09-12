1c0637b31e4cf38ef1ca8c3f31cbd304507b4ff1
Koen Geijtenbeek <k.geijtenbeek@students.uu.nl>
2025-09-12 14:13:00 +0200

    laracasts 1-4 uitwerking

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        about
+    </x-slot:heading>
+    <h1>i am</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>
+        home
+    </x-slot:heading>
+    <h1>hello</h1>
+</x-layout>
+
