c40a00de4ebf9ff93ce6b935cdf94566641d4892
Koen Geijtenbeek <k.geijtenbeek@students.uu.nl>
2025-10-15 10:43:29 +0200

    laracast 13

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,14 +3,17 @@
         job listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-500 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                
+                <div>
                     <strong>{{ $job['title'] }}</strong> salary: {{ $job['salary'] }} per year
-                </a>
-            </li>
+                </div>
+            </a>
+ 
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>
