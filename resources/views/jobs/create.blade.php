951b791dea04c47d547782d3da1721af82bd3f9e
Koen Geijtenbeek <k.geijtenbeek@students.uu.nl>
2025-10-29 10:23:57 +0100

    laracast 17

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,12 +12,15 @@
 
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div class="sm:col-span-4">
-                <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
-                <div class="mt-2">
-                    <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input id="title" type="text" name="title" placeholder="CEO" class="block min-w-0 grow bg-white py-1.5 px-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                    <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                    <div class="mt-2">
+                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                            <input id="title" type="text" name="title" placeholder="CEO" class="block min-w-0 grow bg-white py-1.5 px-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required />
+                        </div>
+                        @error('title')
+                            <p class="text-red-500 font-semibold">{{ $message }}</p>
+                        @enderror
                     </div>
-                </div>
                 </div>
             
 
@@ -26,12 +29,23 @@
                     <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                     <div class="mt-2">
                         <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                            <input id="salary" type="text" name="salary" placeholder="$100.000" class="block min-w-0 grow bg-white py-1.5 px-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                            <input id="salary" type="text" name="salary" placeholder="$100.000" class="block min-w-0 grow bg-white py-1.5 px-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required />
                         </div>
+                        @error('salary')
+                            <p class="text-red-500 font-semibold">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
             </div>
-
+            <!-- <div class="mt-10">
+                @if($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
+            </div> -->
             
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
